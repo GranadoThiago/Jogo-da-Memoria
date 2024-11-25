cb4b3d1b622843a90439c643ca2035f60e61320d
GranadoThiago <learn.now.e@gmail.com>
2024-11-25 15:02:04 -0300

    Dando efeito de rotate nos cards

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -26,6 +26,23 @@ let shuffleEmojis = emojis.sort(() => (Math.random() > 0.5 ? 2 : -1));
 for (let i = 0; i < emojis.length; i++){
     let box = document.createElement("div");
     box.className = "item";
+    box.onclick = handleClick;
     box.innerHTML = shuffleEmojis[i]
     document.querySelector(".game").appendChild(box);
+}
+
+//Função para selecionar os cards
+function handleClick() {
+  if (openCards.length < 2) {
+    this.classList.add("boxOpen");
+    openCards.push(this);
+  }
+  
+  if (openCards.length == 2) {
+    setTimeout(checkMatch, 500);
+  }
+}
+
+function checkMatch(param) {
+  // Tab to edit
 }
